5424060a220707b4cc2e066c9f31743964ac5483
Róbert Mišura <Rbert Miura>
2017-02-28 20:48:27 +0100

    update

--- IPP/CHA.php
+++ IPP/CHA.php
@@ -1,19 +1,51 @@
 <?php
 
-$directory = '/IZP';
+/*
+*	Author: Robert Misura
+*	Project: CHA - C Header Analysis
+*	Subject: IPP - php script
+*	Name: CHA.php
+*/
 
-$it = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($directory));
+//Functions---------------------------------------------------------------------------------------
 
-$it->rewind();
-while($it->valid()) {
+function writeHelp(){
+	echo "HELP MESSAGE:
+	This is PHP script, which analyze C header files (.h). 
+	Script finds functions in files. Allowed arguments:
 
-    if (!$it->isDot()) {
-        echo 'SubPathName: ' . $it->getSubPathName() . "\n";
-        echo 'SubPath:     ' . $it->getSubPath() . "\n";
-        echo 'Key:         ' . $it->key() . "\n\n";
-    }
+	--help              	Prints a help message.
+	--input=fileordir		Input file or directory.
+	--output=file 			Name of output file, default option is STDOUT.		
+	--pretty-xml=k 			Spacing in XML file. Variable k is number of spaces.
+	--no-inline				Scprit ignores functions with inline description.
+	--max-par=n 			Script searches for function with maximum n parameters.
+	--no-duplicates			If file or directory contians same functions, ignores next one.
+	--remove-whitespace		Replaces multiple white spaces for one white space.
 
-    $it->next();
+	Good luck ;) 				
+
+		 ";
+}
+
+function argumentsTest($argv){
+
+	
+}
+
+//-----------------------------------------------------------------------------------------------
+
+
+//argumentsTest($argv);
+
+$file = getopt("input::output::");
+var_dump($file);
+
+
+
+//Errors messages
+function errMsg(){
+	echo "too many argumants \n";
 }
 
 ?>
