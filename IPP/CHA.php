1c37e7643aac2b82df92e0f2b23ccb3666c63ee1
Róbert Mišura <Rbert Miura>
2017-03-06 22:38:49 +0100

    update

--- IPP/CHA.php
+++ IPP/CHA.php
@@ -40,20 +40,20 @@ function argumentsParse($argv,$argc){
  		"remove-whitespace",
  	);
 
- 	$input_path = getcwd();
+ 	$input_path = getcwd(); //default path
  	$dir = "./";
- 	$output_file = STDOUT;
+ 	$output_file = STDOUT; // default output
  	$pretty_xml = false;
- 	$k = -1;
+ 	$k = -1; // p_xmp == false
  	$no_inline = false;
- 	$max_par = false;
+ 	$max_par = -1; //default -1
  	$no_duplicates = false;
  	$remove_whitespace = false;
 
  	if($argc > 1){
  		$arguments = getopt($longopts);
 		if(count($arguments) != ($argc-1))		{
-			fwrite(STRERR, "Invalid format of arguments.");
+			fwrite(STDERR, "Invalid format of arguments.");
 			exit(1);
 		}//if count test
 
@@ -61,7 +61,7 @@ function argumentsParse($argv,$argc){
 			switch($argument){
 				case "help":
 					if(argc != 2){
-						fwrite(STRERR, "Invalid amount of arguments. HELP can t be combined with other arguments");
+						fwrite(STDERR, "Invalid amount of arguments. HELP can t be combined with other arguments");
 						exit(1);
 					}
 				break;
@@ -71,16 +71,47 @@ function argumentsParse($argv,$argc){
 				break;
 
 				case "output":
-					if(!fopen($argument_value, "w")){
-						fwrite(STRERR, "Error: Can t open output file.");
+					if(!(fopen($argument_value, "w"))){
+						fwrite(STDERR, "Error: Can t open output file.");
 						exit(3);
 					}
 				break;
 
 				case "pretty-xml":
-
+					if($argument_value == "")
+						$k = 4;
+					else{
+						if(!is_numeric($argument_value) && (int)$argument_value < 0){
+							fwrite(STDERR, "Error: Pretty-xml must be number bigger than 0.");
+							exit(1);
+						}
+						else{
+							$k = $argument_value;
+						}
+					}
+					$pretty_xml = true;
 				break;
 
+				case "no-inline":
+					$no_inline = true;
+				break;
+
+				case "max-par":
+					if(!is_numeric($argument_value)){
+						fwrite(STDERR, "Error: Argument max-par must be number.");
+						exit(1);
+					}
+
+					$max_par = $argument_value;
+				break;
+
+				case "no-duplicates":
+					$no_duplicates = true;
+				break;
+
+				case "remove-whitespace":
+					$remove_whitespace = true;
+				break;
 
 			}//switch
 		}//foreach
@@ -93,7 +124,6 @@ function argumentsParse($argv,$argc){
 //-----------------------------------------------------------------------------------------------
 
 argumentsParse($argv,$argc);
-
 
 //Errors messages
 function errMsg(){
